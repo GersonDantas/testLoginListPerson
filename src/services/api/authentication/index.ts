deadc89fe76d05bc9275662c02be3016debecaff
gersonDantas <gerson.accari@hotmail.com>
2021-07-27 20:35:37 -0300

    vercel

--- src/services/api/authentication/index.ts
+++ src/services/api/authentication/index.ts
@@ -4,6 +4,5 @@ import { SignInData } from "src/types/index";
 export const handleLogin = async (data: SignInData) => {
   return await api
     .post("/api/v1/Auth/LogIn", data)
-    .then((res) => res.data)
-    .catch((e) => alert(`server error in login ${e.message}`));
+    .then((res) => res.data);
 };
